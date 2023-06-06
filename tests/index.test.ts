65abdbd9ee4610306a16dda72ebde327b39bc14e
qd-qd <qdqdqdqdqd@protonmail.com>
2023-06-06 12:25:02 +0200

    💄 apply linting/formatting to all files

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,7 +1,7 @@
 import sum from "../src";
 
 describe("sum", () => {
-    it("2+2=4", () => {
-        expect(sum(2, 2)).toEqual(4);
-    });
+	it("2+2=4", () => {
+		expect(sum(2, 2)).toEqual(4);
+	});
 });
